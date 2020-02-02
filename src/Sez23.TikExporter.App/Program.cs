d520d208f904247c849a78f5be1287a54a38137e
Frank Binder <frank.binder@serialize.de>
2020-02-02 11:53:10 +0100

    [MOD] App project added, Renamed Namings

---
+++ src/Sez23.TikExporter.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sez23.TikExporter;
+
+namespace Sez23.TikExporter.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var api = new Api.ApiService();
+            api.Read();
+
+
+            Console.WriteLine("TikExporter!");
+        }
+    }
+}
